da7420c0c9f6a6752ecf43e52d9b03b4237c744a
igbt6 <lukasz.uszko@gmail.com>
2016-06-07 01:57:30 +0200

    Test uploading a folder works

--- GoogleDriveSync.py
+++ GoogleDriveSync.py
@@ -1,25 +1,39 @@
-from __future__ import print_function
 import httplib2
 import os
 import sys
 import re
-from apiclient import discovery
+import io
+
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
-import io
 import apiclient
+from apiclient import discovery
 from apiclient.http import MediaFileUpload
 from apiclient.http import MediaIoBaseDownload
 
+
+import logging
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+#logger.setLevel(logging.DEBUG)
+#logging.Logger.disabled = True
+#logging.disable(logging.NOTSET)
+#logger.setLevel(logging.NOTSET)  
+logger.setLevel(logging.NOTSET)
+logger.disabled = False
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'GoogleDriveManager'
 FOLDER_TO_BE_SYNCED_PATH= 'L:\GOOGLE_DRIVE_SYNCER_TEST_FOLDER'
+FILE_TYPE=frozenset(["FILE","FOLDER"])
 
 class GoogleDriveSynchronizer():
-
+    
     def __init__(self,root_folder_name):
         self._root_folder= GoogleDriveFile(root_folder_name)
         self._credentials = self.get_credentials()
@@ -48,7 +62,7 @@ class GoogleDriveSynchronizer():
                 credentials = tools.run_flow(flow, store, flags)
             else: # Needed only for compatibility with Python 2.6
                 credentials = tools.run(flow, store)
-            print('Storing credentials to ' + credential_path)
+            logger.debug('Storing credentials to ' + credential_path)
         return credentials
        
     def find_folder_or_file_by_name(self,file_name,parent_id=None):
@@ -62,19 +76,24 @@ class GoogleDriveSynchronizer():
         while True:
             response = self._service.files().list(q=query,spaces='drive',fields='nextPageToken, files(id, name, parents)',pageToken=page_token).execute()
             for file in response.get('files', []):
-                print('Found file: %s (%s) %s' % (file.get('name'), file.get('id'),file.get('parents')))
+                logger.debug('Found file: %s (%s) %s' % (file.get('name'), file.get('id'),file.get('parents')))
                 return file.get('id')
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 return False;
         
-    def check_if_file_exist_create_new_one(self,file_name,parent_id=None):
+    def check_if_file_exist_create_new_one(self,file_name,file_type="FOLDER",parent_id=None):
+        if file_type not in FILE_TYPE:
+            raise ValueError("Incorrect file_type arg. Allowed types are: %s"%(', '.join(list(FILE_TYPE))))
         id = self.find_folder_or_file_by_name(file_name,parent_id)
         if id:
-            print(file_name + " exists")
+            logger.debug(file_name + " exists")
         else:
-            print(file_name + " does not exist")
-            id=self.create_new_folder(file_name,parent_id)
+            logger.debug(file_name + " does not exist")
+            if file_type is "FILE":
+                pass #TODO
+            else:
+                id=self.create_new_folder(file_name,parent_id)
         return id
         
     
@@ -82,11 +101,11 @@ class GoogleDriveSynchronizer():
         results = self._service.files().list().execute()
         items = results.get('files', [])
         if not items:
-            print('No files found.')
+            logger.debug('No files found.')
         else:
-            print('Files:')
+            logger.debug('Files:')
             for item in items:
-                print('{0} ({1})'.format(item['name'], item['id']))
+                logger.debug('{0} ({1})'.format(item['name'], item['id']))
         
     def create_new_folder(self,folder_name,parent_folders_id =None):
         parent_id= parent_folders_id if parent_folders_id is None else [parent_folders_id]
@@ -96,11 +115,11 @@ class GoogleDriveSynchronizer():
             'parents': parent_id
         }
         file = self._service.files().create(body=file_metadata, fields='id').execute()
-        print('Created Folder ID: %s' % file.get('id'))
+        logger.debug('Created Folder ID: %s' % file.get('id'))
         return file.get('id')
     
-    def insert_file_in_folder(self,file_name,path,parent_folders_id,file_mime_type=None):
-        parent_id= parent_folders_id if parent_folders_id is None else [parent_folders_id]
+    def insert_file_in_folder(self,file_name,path,parent_folder_id,file_mime_type=None):
+        parent_id= parent_folders_id if parent_folder_id is None else [parent_folder_id]
         file_metadata = {
           'name' : file_name,
           'parents': parent_id
@@ -108,59 +127,60 @@ class GoogleDriveSynchronizer():
         media = MediaFileUpload(path,mimetype=file_mime_type,  # if None, it will be guessed 
                                 resumable=True)
         file = self._service.files().create(body=file_metadata,media_body=media,fields='id').execute()
-        print('File ID: %s' % file.get('id'))
+        logger.debug('File ID: %s' % file.get('id'))
+        return file.get('id') 
     
+    def _get_name_and_parent_from_path(self,path):
+        splitted_path= os.path.split(path)
+        file_name=splitted_path[1]
+        parent_folder_name=os.path.split(splitted_path[0])[1]
+        return file_name, parent_folder_name
     
     def create_folders_tree(self,folders):
         if folders is None or len(folders)==0:
             raise ValueError("Incorrect folders format")
-        self.folders_tree=[self._root_folder]
+        self._folders_tree=[self._root_folder]
         #creates folders in root folder
-        # for name, data in folders.items():
-            # id=self.check_if_file_exist_create_new_one(name,self._root_folder.id)
-            # if id is not False:
-                # folder=GoogleDriveFile(name)
-                # folder.id=id
-                # folder.parent_id=self._root_folder.id
-                # self.folders_tree.append(folder)
-            # else:
-               # return  #shall not go into here 
-        #creates rest folders as in root folder
         for name, data in folders.items():
             for path in data["FOLDERS"]:
-                splitted_path= os.path.split(path)
-                folder_name=splitted_path[1]
-                parent_folder_name=os.path.split(splitted_path[0])[1]
-                print(parent_folder_name+" -> "+folder_name)
-                for file in self.folders_tree:
-                    print("------- "+ file.name)
-                    if file.name==parent_folder_name:
-                        folder=GoogleDriveFile(folder_name)
-                        folder.parent_id=file.id
-                        id=self.check_if_file_exist_create_new_one(folder_name,folder.parent_id)
-                        folder.id=id 
-                        self.folders_tree.append(folder)                        
+                folder_name,parent_folder_name=self._get_name_and_parent_from_path(path)
+                logger.debug(parent_folder_name+" -> "+folder_name)
+                for folder in self._folders_tree:
+                    if folder.name==parent_folder_name:
+                        temp_folder=GoogleDriveFile(folder_name)
+                        temp_folder.parent_id=folder.id
+                        id=self.check_if_file_exist_create_new_one(folder_name,parent_id=temp_folder.parent_id)
+                        temp_folder.id=id 
+                        self._folders_tree.append(temp_folder)                        
+    
+    def synchronize_files(self,files_data):
+        if files_data is None or len(files_data)==0:
+            raise ValueError("Incorrect files format")
+        self.create_folders_tree(files_data)
+        for main_folder_name, data in files_data.items():
+            for path in data["FILES"]:
+                file_name,parent_folder_name=self._get_name_and_parent_from_path(path)
+                logger.debug("PATH: "+path+" "+parent_folder_name+" -> "+file_name)
+                for folder in self._folders_tree:
+                    if folder.name==parent_folder_name:
+                        logger.debug("$$$ "+file_name+" "+path+" "+folder.id+" ")
+                        try:
+                            if not self.find_folder_or_file_by_name(file_name,folder.id):
+                                self.insert_file_in_folder(file_name,path,folder.id)
+                        except:
+                            pass #just skip fail
+                        break
                         
-         
-    def main():
-        results = self._service.files().list(pageSize=10,fields="nextPageToken, files(id, name)").execute()
-        items = results.get('files', [])
-        if not items:
-            print('No files found.')
-        else:
-            print('Files:')
-            for item in items:
-                print('{0} ({1})'.format(item['name'], item['id']))
+            
         
-        file_id = '0B0zxuHK9yGdHbml3ZUMtZThrVUU'
+    def download_file(self,file_name,file_id):
         request = self._service.files().get_media(fileId=file_id)
-        #fh = io.BytesIO()
-        fh = io.FileIO('aaaa', 'wb')
-        # downloader = MediaIoBaseDownload(fh, request)
-        # done = False
-        # while done is False:
-            # status, done = downloader.next_chunk()
-            # print("Download %d%%." % int(status.progress() * 100))
+        fh = io.FileIO(file_name, 'wb')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            logger.debug("Download %d%%." % int(status.progress() * 100))
 
             
             
@@ -175,22 +195,17 @@ class FilesFolder():
         self._list_all_filepaths()
         
     def _list_all_filepaths(self):
-        #print(os.listdir(self._root_path))
+        #logger.debug(os.listdir(self._root_path))
+        folders_set=set([])
+        files_set= set([])
         for file_path in os.listdir(self._root_path):
             if os.path.isfile(os.path.join(self._root_path,file_path)):
-                #print(os.path.join(self._root_path,file)) #left files
-                pass
+                files_set.add(os.path.join(self._root_path,file_path))
             else:
                 self._files[file_path]={"PATH":os.path.join(self._root_path,file_path)}
-        #print(self._files) 
         for name,dataDict in self._files.items():
-            folders_set=set([])
-            files_set= set([])
             for root_dir, subdirs, files in os.walk(dataDict["PATH"]):
-                #print([subdir.encode('utf8') for subdir in subdirs])
-                #print([file.encode('utf8') for file in files])
                 folders_set.add(root_dir)
-                #print(root_dir)
                 for file in files:
                         files_set.add(os.path.join(root_dir,file))
                 for subdir in subdirs:
@@ -263,15 +278,14 @@ class FilesFolder():
                     f.write('\n')
             self._files[name]["FOLDERS"]=sorted(folders_set, key=len)
             self._files[name]["FILES"]=list(files_set)
-            # print("\n--------------------PATH---------------------------\n")
-            # print(self._files[name]["PATH"])
-            # print("\n--------------------FOLDERS---------------------------\n")
-            # print(self._files[name]["FOLDERS"])
-            # print("\n--------------------FILES---------------------------\n")
-            # print(self._files[name]["FILES"])
+            # logger.debug("\n--------------------PATH---------------------------\n")
+            # logger.debug(self._files[name]["PATH"])
+            # logger.debug("\n--------------------FOLDERS---------------------------\n")
+            # logger.debug(self._files[name]["FOLDERS"])
+            # logger.debug("\n--------------------FILES---------------------------\n")
+            # logger.debug(self._files[name]["FILES"])
             #break
-
-        
+      
     def extract_folder_name_from_path(self,folder_path):
         m =re.search(r'\\(\w*)$',folder_path)
         if m:
@@ -309,4 +323,7 @@ if __name__ == '__main__':
     # google_drive_syncer.insert_file_in_folder("TestSubFolderFile.txt",os.path.join(FOLDER_TO_BE_SYNCED_PATH,"cowsay.txt"),id)
     
     #TEST2
-    google_drive_syncer.create_folders_tree(file_folder.files)
+    #google_drive_syncer.create_folders_tree(file_folder.files)
+    
+    #TEST3
+    google_drive_syncer.synchronize_files(file_folder.files)
